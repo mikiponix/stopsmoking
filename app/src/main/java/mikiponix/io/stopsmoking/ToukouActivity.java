ef49effedb9495dde76423d2220ad64258398d75
Miki Kawana <miki411moe@gamil.com>
2018-10-06 21:35:06 +0900

    mikimiki

--- app/src/main/java/mikiponix/io/stopsmoking/ToukouActivity.java
+++ app/src/main/java/mikiponix/io/stopsmoking/ToukouActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.Random;
 
 
-public class ToukouActivity extends AppCompatActivity{
+public class ToukouActivity extends AppCompatActivity {
 
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -41,9 +41,42 @@ public class ToukouActivity extends AppCompatActivity{
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.toukou);
+
+
+        String[] advice = {"たまには紅茶にしてみよう",
+                "いいよ、その調子です",
+                "継続は力なり",
+                "大きく深呼吸",
+                "禁煙後20分で血圧や脈が正常化するよ",
+                "１本だけの誘惑に負けないで",
+                "体にいいことしかない！",
+                "辛くなったら頼ってみよう！",
+                "浮いたお金で家族旅行に行こう！",
+                "いつもありがとう！",
+                "今日も元気？一緒に頑張ろう！！",
+                "最初の一週間はつらいけど死なないから大丈夫！",
+                "最近ごはん美味しくない？！味覚や嗅覚、胃の調子が良くなってきてる！",
+                "映画でも観に行こう！",
+                "１本だけお化けにご注意。水の泡に",
+                "5年後肺がんになる確率が半分になるよ",
+                "７２時間後ニコチンが完全に消えるんだよ！続けよう！",
+                "そろそろお金もいい感じじゃない",
+                "吸いたくなったら家族と遊ぼう！",
+                "タバコ臭いが消えた！！！",
+                "ひたすら耐えるのみ！",
+                "味方はたくさんいるよ",
+                "一人じゃないからね",
+                "お金と命があればもう！！",
+                "一緒に長生きしませんか？",
+                "今日のお昼は少し贅沢しよう",
+                "あなたの健康が一番",
+                "２４時間で心臓発作の確率が下がるんだよ！",
+                "２〜３週間で肺活量が３０％回復",
+                "頑張っている人はいつもかっこいい",
+                "つらくなったら相談しよう！甘えじゃない！"};
 
         animationView = (LottieAnimationView) findViewById(R.id.animation_view);
 
@@ -56,11 +89,11 @@ public class ToukouActivity extends AppCompatActivity{
         dayText = (EditText) findViewById(R.id.day_card);
         diaryText = (EditText) findViewById(R.id.diary_card);
         count1Text = (TextView) findViewById(R.id.count1_card);
-        count1 =0;
+        count1 = 0;
         count2Text = (TextView) findViewById(R.id.count2_card);
-        count2 =0;
+        count2 = 0;
         sendButton = (Button) findViewById(R.id.send_button);
-        adviceText = (TextView)findViewById(R.id.advice_card);
+        adviceText = (TextView) findViewById(R.id.advice_card);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,69 +107,61 @@ public class ToukouActivity extends AppCompatActivity{
                 String advice = adviceText.getText().toString();
                 String key = reference.push().getKey();
 
-                Card item = new Card(key, goal, today, day, 1, 2, diary,advice);
+                Card item = new Card(key, goal, today, day, 1, 2, diary, advice);
 
                 reference.child(key).setValue(item).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         finish();
 
-                //reference.child("post").child(key).setValue(item).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    //@Override
-                    //public void onSuccess(Void aVoid) {
+                        //reference.child("post").child(key).setValue(item).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        //@Override
+                        //public void onSuccess(Void aVoid) {
 
 
-                    //}
-                //});
+                        //}
+                        //});
 
-                //reference.child("num").setValue("20").addOnSuccessListener(new OnSuccessListener<Void>() {
-                    //@Override
-                    //public void onSuccess(Void aVoid) {
+                        //reference.child("num").setValue("20").addOnSuccessListener(new OnSuccessListener<Void>() {
+                        //@Override
+                        //public void onSuccess(Void aVoid) {
                         //finish();
-                    //}
-               // });
+                        //}
+                        // });
 
 
+                    }
 
+                });
 
 
             }
 
-        });
-
-
-    }
-
-    public void add(View v){
-        count1 = count1 + 1;
-        count1Text.setText(count1 + "本我慢しました！");
+            public void add(View v) {
+                count1 = count1 + 1;
+                count1Text.setText(count1 + "本我慢しました！");
 
 //        count2 = count2 + 20;
-        count2Text.setText(count1 * 20 + "円貯まりました！！");
+                count2Text.setText(count1 * 20 + "円貯まりました！！");
 
-        Random randomAdvice = new Random();
-        adviceText.setText(advice[randomAdvice.nextInt(31)]);
+                Random randomName = new Random();
+                int index = randomName.nextInt(31);
+                String name = [index];
+                adviceText.setText(advice);
 
 
-    }
+            }
 
-    public void minus(View v){
-        count1 = count1 - 1;
-        count1Text.setText(count1 + "本我慢しました！");
+            public void minus(View v) {
+                count1 = count1 - 1;
+                count1Text.setText(count1 + "本我慢しました！");
 
 //        count2 = count2 - 20;
-        count2Text.setText(count1 * 20 + "円貯まりました!!");
-    }
+                count2Text.setText(count1 * 20 + "円貯まりました!!");
+            }
 
 
-
-
-
-
-
-
-
-                ///public class MainActivity extends AppCompatActivity {
+            ///public class MainActivity extends AppCompatActivity {
 
 //LottieAnimationView animationView;
 
@@ -152,3 +177,7 @@ public class ToukouActivity extends AppCompatActivity{
 //animationView.playAnimation();
 // }
 //}
+        });
+        return;
+    }
+}
